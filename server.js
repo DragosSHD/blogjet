e27d2bee906fbf3c0e63b5e3c60909079e358860
DragosSHD <dragos.dragos.502@gmail.com>
2022-02-01 18:28:28 +0200

    Connect to the database.

--- server.js
+++ server.js
@@ -20,5 +20,18 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-
+// Connect to the database
+const db = require('./app/models')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    })
 
