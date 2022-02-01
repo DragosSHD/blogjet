e545e541480fe8226c085ce0e5c434ac7e08bd8e
DragosSHD <dragos.dragos.502@gmail.com>
2022-02-01 17:15:04 +0200

    Set project file structure.

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const express = require("express")
+// Imports
+const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to Blogjet!"})
-})
+// Static files
+app.use(express.static('./app/public'));
+app.use('/css', express.static(__dirname + 'app/public/css'));
+app.use('/js', express.static(__dirname + 'app/public/js'));
+app.use('/img', express.static(__dirname + 'app/public/img'));
 
+// Set template engine
+app.set('view engine', 'ejs');
+
+// Set demo message
+app.get("/", (req, res) => {
+    res.json({message: "Welcome to Blogjet!"});
+});
+
+// Listen to port 8080
 const PORT = 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
