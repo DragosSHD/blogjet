d345d8a61a8de1d49d5bc68d6c82114ac4f061de
DragosSHD <dragos.dragos.502@gmail.com>
2022-02-02 15:47:51 +0200

    Fix typo in post.controller.js

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -4,7 +4,7 @@ const Post = db.post;
 // Create and Save a new Post
 exports.create = (req, res) => {
 // Validate request
-    if (!req.body.name) {
+    if (!req.body.title) {
         res.status(400).send({ message: "Content can not be empty!" });
         return;
     }
