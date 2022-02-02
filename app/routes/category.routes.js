b51ac04406a1eb2f6d5472329dc122c858cfd308
DragosSHD <dragos.dragos.502@gmail.com>
2022-02-02 12:09:32 +0200

    Implement Category routes.

---
+++ app/routes/category.routes.js
@@ -0,0 +1,20 @@
+module.exports = app => {
+    const category = require("../controllers/category.controller");
+    const router = require("express").Router;
+
+    // CRUD Operations
+    // CREATE new category
+    router.post("/", category.create);
+    // RETRIEVE ALL categories
+    router.get("/", category.findAll);
+    // RETRIEVE a single category based on id
+    router.get("/:id", category.findOne);
+    // UPDATE category based on id
+    router.put("/:id", category.update);
+    // DELETE a category based on id
+    router.delete("/:id", category.delete);
+    // DELETE all categories
+    router.delete("/", category.deleteAll);
+
+  app.use("/api/category", router);
+};
