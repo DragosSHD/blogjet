fe354912ed6ec86e5dd361a478963977f9c38a7c
DragosSHD <dragos.dragos.502@gmail.com>
2022-02-06 17:03:47 +0200

    Handle 404 errors.

--- app/routes/website.routes.js
+++ app/routes/website.routes.js
@@ -29,4 +29,25 @@ module.exports = app => {
 
 
     app.use('', router);
+  // Handle 404 errors
+  app.use(function(req, res, next) {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('pages/404', {
+        title: "404 - Not found"
+      });
+      return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+      res.json({ error: 'Not found' });
+      return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+  });
 };
